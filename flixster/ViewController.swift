4fc23cd58eeaef7f44f2ce655b3d06209a9b24bd
Nur Nibir <nurnibir123@gmail.com>
2021-02-17 20:10:09 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Nur Nibir on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
